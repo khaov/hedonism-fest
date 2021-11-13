cc2dcc5ef3ffa0a7c0d25d26dc21359dd7f580a2
AlexanderSub <subbotin_aleks@mail.ru>
2021-11-13 15:36:04 +0300

    изменение кнопок меню

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,8 @@
 const menuPopup = document.querySelector('.menu_primary')
 const openMenuButton = document.querySelector('.button_type_header_menu')
 const menuCityPopup = document.querySelector('.menu_city')
-const openMenuCityButton = document.querySelector('.menu__arrow-button_right')
-const closeMenuCityButton = document.querySelector('.menu__arrow-button_left')
+const openMenuCityButton = document.querySelector('.menu__choose-city')
+const closeMenuCityButton = document.querySelector('.menu__goback')
 
 function openMenu() {
   menuPopup.classList.toggle('menu__opened')
